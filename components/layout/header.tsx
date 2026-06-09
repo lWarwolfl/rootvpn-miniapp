54e8def94571a953b4fbc30534e7c81fd11a3e51
lWarwolfl <sinakheiri.dev@gmail.com>
2026-06-09 14:25:46 +0330

    fix: rounded header

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -4,7 +4,7 @@ import { WebAppUser } from '@twa-dev/types'
 
 export default function Header({ user }: { user: WebAppUser | undefined }) {
   return (
-    <div className="from-background sticky top-0 left-0 flex h-10 w-[101%] items-end justify-between gap-4 bg-linear-to-b to-transparent">
+    <div className="from-background sticky top-0 left-0 flex h-10 w-[101%] items-end justify-between gap-4 rounded-t-xl bg-linear-to-b to-transparent">
       <Badge variant="secondary">
         {user?.first_name}
         {user?.last_name ? ` ${user.last_name}` : null}
